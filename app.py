a25094943af032b295a9604b1bdebfeaee8e8c70
admin <D.Alavaro@gmail.com>
2022-12-05 12:46:25 +0300

    05.11.2022 Improved to 24 homework Added function regex_query Added typing

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import json
 import os
+from typing import Callable, Optional
 
-from constants import BASE_DIR
-from flask import Flask, request, jsonify, render_template
+from constants import BASE_DIR, DATA_DIR
+from flask import Flask, request, jsonify, render_template, Response
 from utils import dict_of_utils, log_generator
+from typing import Iterator
 
 app = Flask(__name__)
 
@@ -15,35 +17,41 @@ app = Flask(__name__)
 
 
 @app.get('/')
-def index():
+def index() -> str:
     return render_template("index.html")
 
 
 
 
 @app.post("/perform_query")
-def perform_query():
-    file_name = request.form.get('file_name')
-    cmd1 = request.form.get('cmd1')
-    value1 = request.form.get('value1')
-    cmd2 = request.form.get('cmd2')
-    value2 = request.form.get('value2')
+def perform_query() -> str | Response:
+    file_name: Optional[str] = request.form.get('file_name')
+    cmd1: str = request.form['cmd1']
+    value1: str = request.form['value1']
+    cmd2: str = request.form['cmd2']
+    value2: str = request.form['value2']
 
-    if None in (file_name, cmd1, value1, cmd2, value2):
-        return 'Не все поля заполнены', 400
+    if not all((file_name, cmd1, value1, cmd2, value2)):
+        return Response('Не все поля заполнены', status=400)
+
+    if file_name is None:
+        file_name = DATA_DIR
 
     if not os.path.exists(BASE_DIR + '/data/' + file_name):
-        return 'Файл не найден', 400
+        return Response('Файл не найден', status=400)
 
     default_generator = log_generator()
 
-    first_func = dict_of_utils.get(cmd1)
-    second_func = dict_of_utils.get(cmd2)
+    first_func: Optional[Callable] = dict_of_utils.get(cmd1)
+    second_func: Optional[Callable] = dict_of_utils.get(cmd2)
 
-    first_res = first_func(value1, default_generator)
-    second_res = second_func(value2, first_res)
+    result: Optional[Iterator[str]] = None
+    if first_func is not None:
+        result = first_func(param=value1, generate=default_generator)
+        if second_func is not None:
+            result = second_func(param=value2, generate=result)
 
-    return render_template("block.html", items=second_res)
+    return render_template("block.html", items=result)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,48 +1,39 @@
+import re
+from typing import Iterator, Any, Callable
 from constants import DATA_DIR
-import ipaddress
 
 
-def log_generator():
+def log_generator() -> Iterator:
     with open(DATA_DIR) as file:
-        log_sting = file.readlines()
+        log_sting: list[str] = file.readlines()
         for log in log_sting:
             yield log
 
 
-def user_filter(value, generate):
-    return filter(lambda x: value in x, generate)
+def user_filter(param: str, generate: Iterator[str]) -> Iterator[str]:
+    return filter(lambda x: param in x, generate)
 
 
-def user_map(num, generate):
-    return map(lambda string: string.split()[int(num)], generate)
+def user_map(param: int, generate: Iterator[str]) -> Iterator[str]:
+    return map(lambda string: string.split()[int(param)], generate)
 
 
-def user_unique(value, generate):
-    seen = set()
-    for row in generate:
-        if row in seen:
-            continue
-        else:
-            seen.add(row)
-            yield row
+def user_unique(generate: Iterator[str], *args: Any, **kwargs: Any) -> Iterator[str]:
+    listt: list[str] = []
+    for string in generate:
+        if string not in listt:
+            listt.append(string)
+            yield string
 
 
-def user_sort(order, generate):
-    # reverse = None
-    #
-    # if order == 'asc':
-    #     reverse = False
-    # elif order == 'desc':
-    #     reverse = True
-
-    reverse = order == 'desc'
-    return iter(sorted(generate, reverse=reverse))
+def user_sort(param: str, generate: Iterator[str]) -> Iterator[str]:
+    return iter(sorted(generate, reverse=param == 'desc'))
 
 
-def user_limit(num, generate):
+def user_limit(param: str, generate: Iterator[str]) -> Iterator[str]:
     counter = 1
     for string in generate:
-        if counter > num:
+        if counter > int(param):
             break
 
         counter += 1
@@ -50,10 +41,16 @@ def user_limit(num, generate):
         yield string
 
 
-dict_of_utils = {
+def regex_query(param: str, generator: Iterator[str]) -> Iterator[str]:
+    pattern: re.Pattern = re.compile(param)
+    return filter(lambda x: re.search(pattern, x), generator)
+
+
+dict_of_utils: dict[str, Callable[..., Iterator[str]]] = {
     'filter': user_filter,
     'map': user_map,
     'unique': user_unique,
     'sort': user_sort,
-    'limit': user_limit
+    'limit': user_limit,
+    'regex': regex_query,
 }
